c3d1fca234515544599ed0490ae5dba754c4b08d
Tito <61942255+TitoManri@users.noreply.github.com>
2023-09-26 09:20:38 -0600

    Update SC304_3C2023_G3.java
    Plantilla

--- SC304_3C2023_G3/src/main/java/com/mycompany/sc304_3c2023_g3/SC304_3C2023_G3.java
+++ SC304_3C2023_G3/src/main/java/com/mycompany/sc304_3c2023_g3/SC304_3C2023_G3.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sc304_3c2023_g3;
 
-/**
- *
- * @author manri
- */
+
 public class SC304_3C2023_G3 {
 
     public static void main(String[] args) {
