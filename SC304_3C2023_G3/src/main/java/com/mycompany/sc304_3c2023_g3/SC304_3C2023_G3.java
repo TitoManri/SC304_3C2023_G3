dc4eee67c79d8963cf8f662a9dc25a324df309ef
Tito <61942255+TitoManri@users.noreply.github.com>
2023-09-26 09:19:59 -0600

    Add files via upload

---
+++ SC304_3C2023_G3/src/main/java/com/mycompany/sc304_3c2023_g3/SC304_3C2023_G3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sc304_3c2023_g3;
+
+/**
+ *
+ * @author manri
+ */
+public class SC304_3C2023_G3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
