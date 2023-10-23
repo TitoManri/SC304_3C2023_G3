739d548f5a979cd140583a183fcd46327a9b182a
anapng <134026852+anapng@users.noreply.github.com>
2023-10-22 20:34:06 -0600

    Update
    Prueba

--- SC304_3C2023_G3/src/main/java/com/mycompany/sc304_3c2023_g3/Restaurante/Platillo.java
+++ SC304_3C2023_G3/src/main/java/com/mycompany/sc304_3c2023_g3/Restaurante/Platillo.java
@@ -1,7 +1,10 @@
 package com.mycompany.sc304_3c2023_g3.Restaurante;
 
 public class Platillo extends Catalogo {
-    
+    protected String platosFuertes;
+    protected String ensaldas;
+    protected String entradas;
+    protected String sopas;
     
     public Platillo() {
         super();
